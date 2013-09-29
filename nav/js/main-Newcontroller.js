b3387ba817e72e053673a2441669e24c62574de8
Ali Rokni <Ali@Alis-MacBook-Pro.local>
2013-09-28 23:15:45 -0700

    updated templates
    updated path, added google fonts api

--- nav/js/main-Newcontroller.js
+++ nav/js/main-Newcontroller.js
@@ -3,15 +3,16 @@
 var  sys = require('sys'),
     http = require('http'),
       fs = require('fs'),
-Mustache = require('../../node_modules/mustache'),
+Mustache = require('../node_modules/mustache'),
     path = require('path'),
 template = "./mysite.html";  // relative to its execution path
 
   var siteData = JSON.parse(fs.readFileSync("./site-dataModel.json", "utf8"));
-  
+  //var articleData = JSON.parse(fs.readFileSync("./article-dataModel.json", "utf8"));
+
 http.createServer(function (request, response) {
 
-  var filePath = '..' + request.url;
+  var filePath = '.' + request.url;
       if (filePath == './') {
           filePath = './mysite.html';
       }
@@ -35,21 +36,17 @@ http.createServer(function (request, response) {
 
             if (error) {
               response.writeHead(500);
-              response.write(err + "\n");
               response.end();
-              return;
             } else {
               response.writeHead(200, { 'Content-Type': contentType });
               response.write(Mustache.to_html(template.toString(), siteData, "utf8"));
+              //response.write(Mustache.to_html(template, articleData, "utf8"));
               response.end();
             }
           });
         } else {
-          console.log("not exists: " + filePath);
-          response.writeHead(404, {'Content-Type': 'text/plain'});
-          response.write('404 Not Found\n');
+          response.writeHead(404);
           response.end();
-          return;
         }
       });
     }).listen(1337);
