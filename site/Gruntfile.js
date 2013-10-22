ca0aeba47af73fd2f4720cb716cd65702809e831
Ali Rokni <Ali@Alis-MacBook-Pro.local>
2013-10-22 01:18:25 -0700

    Updated templates
    Updated Gruntfile.js to copy relase directory from build directory

--- site/Gruntfile.js
+++ site/Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
 
 	    // Validate files with JSHint
 	    jshint: {
- 			files: 	['build/js/site-script.js','build/js/delicious.js'],
+ 			files: 	[ 'build/js/site-script.js','build/js/delicious.js' ],
 		    options: {
 				"-W065": true, // radix param for parsInt
 		        "globals": {
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
         	},
 			js: {
 		    	// the files to concatenate
-		    	src: ['build/js/*.js', 'build/js/**/*.js', '!build/js/highchartjs/**/*', '!build/js/jquery*', '!build/js/modernizr*', '!build/js/site-dataModel*'],
+		    	src: [ 'build/js/*.js', 'build/js/**/*.js', '!build/js/highchartjs/**/*', '!build/js/jquery*', '!build/js/modernizr*', '!build/js/site-dataModel*' ],
 				// the location of the resulting JS file
 				dest: 'build/js/script.concat.js'
 			}
@@ -37,7 +37,7 @@ module.exports = function(grunt) {
 		    		paths: ["css"]
 		    	},
 		    	files: {
-		    		'build/css/style.css': ['build/css/*.less', 'build/css/**/*.less']
+		    		'build/css/style.css': [ 'build/css/*.less', 'build/css/**/*.less' ]
 		    	}
 		  	},
 		  	release: {
@@ -46,7 +46,7 @@ module.exports = function(grunt) {
 		    		yuicompress: true
 		    	},
 		    	files: {
-		    		'release/css/style.min.css': ['build/css/*.less', 'build/css/**/*.less']
+		    		'release/css/style.min.css': [ 'build/css/*.less', 'build/css/**/*.less' ]
 		    	}
 		  }
 		},
@@ -61,7 +61,7 @@ module.exports = function(grunt) {
 				}
 			}
 	    },
-	    //Optimize png and jpag.
+	    //Optimize png and jpg
 		imagemin: {
 	    	options: { 
 	    		optimizationLevel: 3
@@ -72,26 +72,43 @@ module.exports = function(grunt) {
 	        }
 		},
 		clean: {
-			build: ['build/js/script.concat.js'],
-		  	release: ['release/css/style.min.css','release/js/script.min.js']
+			build: [ 'build/js/script.concat.js' ],
+		  	release: [ 'release/css/style.min.css','release/js/script.min.js' ]
+		},
+		copy: {
+		  main: {
+			options:{
+				processContent: function(content, srcpath) {  
+			    	content = content.replace(/\/\/@ sourceMappingURL=jquery.min.ma/, '//-@-sourceMappingURL=jquery.min.ma');   
+			    	return content;  
+				}
+			},
+		    files: [
+		      {
+		      	expand: true,
+		      	cwd: 'build/',
+		      	src: [ 'view/*', 'js/jquery.min.js', 'js/modernizr.min.js', 'js/high*/**/*.js','!js/**/*.src.js', 'js/site-dataModel.json', 'images/*' ],
+		      	dest: 'release'
+		      }    
+		    ]
+		  }
 		},
 	    watch: {
 	    	options: {
     			livereload: true
   			},
 	    	js: {
-		      	files: ['build/js/**/*.js', 'Gruntfile.js', 'app.js'],
-		      	tasks: ['default'] 		
+		      	files: [ 'build/js/**/*.js', 'Gruntfile.js', 'app.js' ],
+		      	tasks: [ 'default' ]
 	    	},
 	    	css: {
 	    		files: [ 'build/css/*.less' ],
 	      		tasks: [ 'default' ]
 	    	},
  			html: {
-            	files: [ 'build/view/index.html', 'release/view/index.html']
+            	files: [ 'build/view/index.html', 'release/view/index.html' ]
         	}
-	    }	
-
+	    }
     });
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -102,6 +119,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['clean','jshint', 'concat', 'less', 'uglify', 'imagemin', 'watch']);
+
+	grunt.registerTask('default', [ 'clean', 'jshint', 'concat', 'less', 'uglify', 'imagemin', 'copy','watch' ]);
 }
 
