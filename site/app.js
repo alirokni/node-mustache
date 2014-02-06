6a2a1d47d16f3c080edcdf82d73c2b9c8430810c
Ali Rokni <alirokni@gmail.com>
2014-02-06 00:15:24 -0800

    Updated app for clarity
    Updated contentType, path and added variable

--- site/app.js
+++ site/app.js
@@ -1,68 +1,55 @@
 // main.js and can be relative to the template
 
 var  sys = require('sys'),
-    http = require('http'),
-      fs = require('fs'),
-Mustache = require('../node_modules/mustache'),
-    path = require('path'),
-template = '';
+     http = require('http'),
+       fs = require('fs'),
+ Mustache = require('mustache'),
+     path = require('path'),
+ template = '',
+appServer = '';
 
 // Model, json data,  site content
 var siteData = JSON.parse(fs.readFileSync("release/js/site-dataModel.json", "utf8"));
   
-http.createServer(function (request, response) {
+appServer = http.createServer(function (request, response) {
 
   var filePath = request.url;  // relative to its execution path
+
   if ( filePath.indexOf('html') != -1 ) { 
-	      filePath = 'release/view' + filePath; // checking html
-	} else {
-        filePath = 'release/' + (request.url).substring(1);  // To remove extra / in path for non html files
-	}
+    filePath = 'release/view' + filePath; // checking html
+  } else {
+    filePath = 'release/' + (request.url).substring(1);  // To remove extra / in path for non html files
+  }
 
-  var extname = path.extname(filePath),
-      contentType = 'text/html;charset=utf-8;';
-
-      switch (extname) {
-          case '.js':
-            contentType = 'text/javascript';
-          break;
-          case '.json':
-            contentType = 'text/json';
-          break;
-          case '.css':
-            contentType = 'text/css';
-          break;
-          case '.png':
-            contentType = 'image/png';
-          break;
-          case '.gif':
-            contentType = 'image/gif';
-          break;
-          case '.svg':
-            contentType = 'image/svg+xml';
-          break;
-          case '.ico':
-            contentType = 'image/x-icon';
-          break;
-
-      }
+  var fileExtName = path.extname(filePath),
+      contentType = {
+        'html': 'text/html',
+        'js':   'text/javascript',
+        'css':  'text/css',
+        'json': 'text/json',
+        'png':  'image/png',
+        'svg':  'image/svg+xml',
+        'ico':  'image/x-icon',
+        'gif':  'image/gif'
+      },
+      contentType = contentType[fileExtName.substring(1)] || 'text/html';
 
       fs.exists(filePath, function(exists) {
         if (exists) {
           fs.readFile(filePath, function(error,template) {
             if (error) {
-      			  response.writeHead(500, {'Content-Type': 'text/plain;charset=utf-8'});
+              response.writeHead(500, {'Content-Type': 'text/plain;charset=utf-8'});
               response.write('500 - Interanl Service Error \n' + error );
               response.end();
               return;
             } else {
               response.writeHead(200, { 'Content-Type': contentType});
               if( contentType === 'image/png' || contentType === 'image/x-icon' ){
-			  	      response.write(new Buffer(template));
+                response.write(new Buffer(template));
               } else {
-              	response.write(Mustache.render(template.toString(), siteData, "utf8"));
+                response.write(Mustache.render(template.toString(), siteData, "utf8"));
               }
-			        response.end();
+              response.end();
               return;
             }
           });
@@ -74,5 +61,7 @@ http.createServer(function (request, response) {
           return;
         }
       });
-    }).listen(1337);
-    console.log('Server running at http://127.0.0.1:1337/');
+    });
+
+appServer.listen(1337, "localhost");
+console.log('Server running at http://127.0.0.1:1337/');
